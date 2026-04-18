d0b5a1fa773d102216d9cf7e7b8744742fee6693
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-04-18 12:36:14 +0300

    Indicated constants to show where the movie cards will be posted

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const API_KEY = "5a6e3de1cadd2f14d90edffa614e4263";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
+const heromovie = document.getElementById ("heromovie");
+
+const trending1 = document.getElementById ("trending1");
+const trending2 = document.getElementById ("trending2");
+const trending3 = document.getElementById ("trending3");
+
+const toprated1 = document.getElementById ("toprated1");
+const toprated2 = document.getElementById ("toprated2");
+const toprated3 = document.getElementById ("toprated3");
