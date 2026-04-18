402bd5b49f49229a78bdc59fef95b0ed610a4719
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-04-18 13:02:33 +0300

    Added a hero section movie

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const API_KEY = "5a6e3de1cadd2f14d90edffa614e4263";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
+/* I am indicating the following constants below to let js know that these will be the places where the movie will be posted*/
 const heromovie = document.getElementById ("heromovie");
 
 const trending1 = document.getElementById ("trending1");
@@ -11,3 +12,21 @@ const trending3 = document.getElementById ("trending3");
 const toprated1 = document.getElementById ("toprated1");
 const toprated2 = document.getElementById ("toprated2");
 const toprated3 = document.getElementById ("toprated3");
+
+async function getHeroMovie() {
+  const response = await fetch(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
+  );
+
+  const data = await response.json();
+
+  const movie = data.results[0];
+
+  heromovie.innerHTML = `
+    <img src="${IMAGE_URL + movie.poster_path}" alt="${movie.title}">
+    <h3>${movie.title}</h3>
+    <p>⭐ ${movie.vote_average}</p>
+  `;
+}
+
+getHeroMovie();
